ce9a4f68856cf42147736f9d0570984326277dd0
MHulfeldW <michael.hulfeld-wood@georgebrown.ca>
2022-04-05 17:20:10 -0400

    More cleaning up

--- SDLGame/Game.cpp
+++ SDLGame/Game.cpp
@@ -404,14 +404,9 @@ void Game::spawnEnemyBullets(const float deltaTime)
 			{ 
 				/*timeBeforeNextEnemyShot = timeBetweenEnemyShots;*/
 
-				/*int projectilesPerShot = 3;
-				float spread = 1.0f;
-				float bulletSpeed = 400.0f;
-				for (int i = 0; i < projectilesPerShot; i++)
-				{
-					float angle = ((spread / (projectilesPerShot - 1)) * i) + (spread);*/
+				
 
-					std::cout << "shoot!" << std::endl;
+					/*std::cout << "shoot!" << std::endl;*/
 					Enemy_Bullet* pNewEnemyBullet = new Enemy_Bullet(pRenderer); // the new keyword creates an instance of that class type, and returns a pointer to it
 					// The danger of the new keyword is that we are now responsible for deallocating the memory for this object with the keyword delete
 					Sprite* pEnemyBulletCastedToSprite = (Sprite*)pNewEnemyBullet; // cast from child class to base class pointer
@@ -420,19 +415,9 @@ void Game::spawnEnemyBullets(const float deltaTime)
 															pSprite->position.y + pSprite->getSize().y
 					};
 
-					/*Vector2 launchVelocity = Vector2{ cos(angle) * bulletSpeed,
-													  sin(angle) * bulletSpeed
-					};*/
-
 					pNewEnemyBullet->position = launchPosition;
-					/*pNewEnemyBullet->velocity = launchVelocity;*/
 
 					sprites.push_back(pEnemyBulletCastedToSprite);
-
-					
-					
-					
-				/*}*/
 				
 			}
 		/*	timeBeforeNextEnemyShot -= deltaTime;*/
